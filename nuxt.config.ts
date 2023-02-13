3798a2709985e63aa77cf52f1c407e31084efa66
Leonardo Nadin <nadin.leonardo@gmail.com>
2023-02-12 22:58:02 -0300

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+import postcss from "postcss";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ['@nuxtjs/tailwindcss'],
+    css: [
+        '@/assets/css/main.css',
+    ],
+});
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="bg-slate-900">
+        <div class="container mx-auto">
+            <div class="flex flex-col items-center justify-center min-h-screen py-2">
+                <div class="flex flex-col items-center justify-center w-full flex-1 px-20">
+                    <div>
+                        <h1 class="text-6xl font-bold bg-gradient-to-r bg-clip-text bg-transparent from-sky-300 to-teal-300 text-fill-transparent">
+                            Leonardo<br />
+                            Nadin
+                        </h1>
+                        <p class="mt-3 text-2xl text-white">
+                            Full Stack Developer
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
